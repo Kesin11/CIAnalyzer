54b92ee092fc6267e7cd5a2dfa3ad9a08e67b654
Kenta Kase <kesin1202000@gmail.com>
2020-04-29 16:20:03 +0900

    Attach workflow name

--- src/client/github_client.ts
+++ src/client/github_client.ts
@@ -9,20 +9,44 @@ export class GithubClient {
     })
   }
 
-  async fetchWorkflows(owner: string, repo: string, fromRunId?: number) {
+  async fetchWorkflowRuns(owner: string, repo: string, fromRunId?: number) {
     // TODO: loop and increment page while found fromRunId
     const runs = await this.octokit.actions.listRepoWorkflowRuns({
       owner,
       repo,
+      status: "completed",
       // per_page: 100, // default
       // page: 1, // order desc
     })
 
-    const workflowRuns = runs.data.workflow_runs
+    const workflows = await this.fetchWorkflows(owner, repo)
+    const workflowIdMap = new Map((
+      workflows.map((workflow) => [String(workflow.id), workflow.name])
+    ))
+
+    // Attach workflow name
+    const workflowRuns = runs.data.workflow_runs.map((run) => {
+      const workflowId = run.workflow_url.split('/').pop()! // parse workflow_url
+      return {
+        name: workflowIdMap.get(workflowId)!,
+        run: run
+      }
+    })
+
     return (fromRunId)
-      ? workflowRuns.filter((run) => run.id > fromRunId)
+      ? workflowRuns.filter((workflowRun) => workflowRun.run.id > fromRunId)
       : workflowRuns
   }
+
+  async fetchWorkflows(owner: string, repo: string) {
+    const workflows = await this.octokit.actions.listRepoWorkflows({
+      owner,
+      repo,
+    })
+
+    return workflows.data.workflows
+  }
+
 
   async fetchJobs(owner: string, repo: string, runId: number) {
     const jobs = await this.octokit.actions.listJobsForWorkflowRun({
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { GithubClient } from './client/github_client'
+import { GithubAnalyzer } from './analyzer/github_analyzer'
 
 const main = async () => {
   const GITHUB_TOKEN = process.env['GITHUB_TOKEN'] || ''
@@ -6,10 +7,14 @@ const main = async () => {
   const REPO = 'Firestore-simple'
   const githubClient = new GithubClient(GITHUB_TOKEN)
 
-  const runs = await githubClient.fetchWorkflows(OWNER, REPO)
+  const runs = await githubClient.fetchWorkflowRuns(OWNER, REPO)
   console.dir(runs)
 
-  const jobs = await githubClient.fetchJobs(OWNER, REPO, runs[0].id)
+  const jobs = await githubClient.fetchJobs(OWNER, REPO, runs[0].run.id)
   console.dir(jobs)
+
+
+  const githubAnalyzer = new GithubAnalyzer()
+  githubAnalyzer.createWorkflowReport(runs[0].name, runs[0].run, jobs)
 }
 main()
