c672c23a5a1084a771c0c100ec6b7cdfa8687619
Kenta Kase <kesin1202000@gmail.com>
2020-05-09 14:56:01 +0900

    Add CLI option parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import yargs from 'yargs'
 import { loadConfig } from './config/config'
 import { CompositRunner } from './runner/runner'
 
+const argv = yargs
+  .command(['$0', 'workflow'], 'Collect workflow data from CI services', (yargs) => {}, () => {})
+  .options({
+    c: { type: 'string', alias: 'config', describe: 'Path to config yaml' },
+    v: { type: 'count', alias: 'verbose' },
+  })
+  .strict()
+  .argv
+
 const main = async () => {
-  const yamlConfig = loadConfig()
+  const yamlConfig = loadConfig(argv.c)
 
   const runner = new CompositRunner(yamlConfig)
   await runner.run()
