dddf93c58187fa59f4e9a89ac3b6051b284c0540
Kenta Kase <kesin1202000@gmail.com>
2020-05-14 01:00:52 +0900

    fix(test): Fix last_run_store test and add local_store test

--- __tests__/last_run_store.test.ts
+++ __tests__/last_run_store.test.ts
@@ -1,50 +1,27 @@
 import { LastRunStore } from '../src/last_run_store'
-import os from 'os'
-import fs from 'fs'
-import path from 'path'
-import crypto from 'crypto'
+import { Store, AnyObject } from '../src/store/store'
 
-const createRandomStr = () => crypto.randomBytes(8).toString('hex')
+class MockStore implements Store {
+  constructor (public data: AnyObject = {}) { }
+  async read<T extends AnyObject>(): Promise<T> {
+    return this.data as T
+  }
+  async write<T extends AnyObject> (newStore: T): Promise<T> {
+    this.data = { ...newStore }
+    return this.data as T
+  }
+}
 
 describe('LastRunStore', () => {
   const repo = 'owner/repo'
-  let storePath: string
+  let mockStore: MockStore
+  let lastRunStore: LastRunStore
   beforeEach(() => {
-    storePath = path.join(
-      os.tmpdir(),
-      `${createRandomStr()}.json`
-    )
-  })
-  afterEach(async () => {
-    try {
-      await fs.promises.access(storePath)
-      await fs.promises.unlink(storePath)
-    } catch {}
-  })
-
-  describe('new', () => {
-    it('this.store should empty object when store file not found', async () => {
-      const actual = await LastRunStore.init('github', storePath)
-      expect(actual.lastRun).toEqual({})
-    })
-
-    it('this.store should same as last run store json', async () => {
-      const now = new Date()
-      const lastRun = {
-        'owner/repo': {
-          lastRun: 1,
-          updatedAt: now.toISOString(),
-        }
-      }
-      await fs.promises.writeFile(storePath, JSON.stringify(lastRun))
-
-      const actual = await LastRunStore.init('github', storePath)
-      expect(actual.lastRun).toEqual(lastRun)
-    })
+    mockStore = new MockStore()
+    lastRunStore = new LastRunStore(mockStore)
   })
 
   it('getLastRun', async () => {
-    const lastRunStore = await LastRunStore.init('github', storePath)
     lastRunStore.lastRun[repo] = {
       lastRun: 100,
       updatedAt: new Date()
@@ -54,25 +31,22 @@ describe('LastRunStore', () => {
   })
 
   describe('setLastRun', () => {
-    it('should accept when value is undefined', async () => {
-      const lastRunStore = await LastRunStore.init('github', storePath)
+    it('should accept when current lastRun is undefined', async () => {
       lastRunStore.setLastRun(repo, 100)
 
       expect(lastRunStore.lastRun[repo].lastRun).toEqual(100)
     })
 
-    it('should accept when value is less than arg', async () => {
+    it('should accept when current lastRun is less than arg', async () => {
       const lastRun = 100
-      const lastRunStore = await LastRunStore.init('github', storePath)
       lastRunStore.setLastRun(repo, 99)
       lastRunStore.setLastRun(repo, lastRun)
 
       expect(lastRunStore.lastRun[repo].lastRun).toEqual(lastRun)
     })
 
-    it('should not accept when value is larger than arg', async () => {
+    it('should not accept when current lastRun is larger than arg', async () => {
       const lastRun = 100
-      const lastRunStore = await LastRunStore.init('github', storePath)
       lastRunStore.setLastRun(repo, 101)
       lastRunStore.setLastRun(repo, lastRun)
 
@@ -81,16 +55,13 @@ describe('LastRunStore', () => {
   })
 
   it('save', async () => {
-    const lastRunStore = await LastRunStore.init('github', storePath)
     lastRunStore.setLastRun(repo, 100)
     await lastRunStore.save()
 
-    const writedFile = await fs.promises.readFile(storePath)
-    const actual = JSON.parse(writedFile.toString())
-    expect(actual).toStrictEqual({
-      'owner/repo': {
+    expect(mockStore.data).toEqual({
+      [repo]: {
         lastRun: 100,
-        updatedAt: expect.anything()
+        updatedAt: expect.anything(),
       }
     })
   })
---
+++ __tests__/store/local_store.test.ts
@@ -0,0 +1,68 @@
+import { LocalStore } from '../../src/store/local_store'
+import os from 'os'
+import fs from 'fs'
+import path from 'path'
+import crypto from 'crypto'
+
+const createRandomStr = () => crypto.randomBytes(8).toString('hex')
+
+describe('LocalStore', () => {
+  const repo = 'owner/repo'
+  let storePath: string
+  let localStore: LocalStore
+  beforeEach(() => {
+    storePath = path.join(
+      os.tmpdir(),
+      `${createRandomStr()}.json`
+    )
+    localStore = new LocalStore('test', storePath)
+  })
+  afterEach(async () => {
+    try {
+      await fs.promises.access(storePath)
+      await fs.promises.unlink(storePath)
+    } catch {}
+  })
+
+  describe('read', () => {
+    it('should return same object as writed JSON', async () => {
+      const now = new Date()
+      const lastRun = {
+        'owner/repo': {
+          lastRun: 1,
+          updatedAt: now.toISOString(),
+        }
+      }
+      await fs.promises.writeFile(storePath, JSON.stringify(lastRun))
+
+      const actual = await localStore.read()
+      expect(actual).toEqual(lastRun)
+    })
+
+    it('should return empty object when JSON was not found', async () => {
+      const actual = await localStore.read()
+      expect(actual).toEqual({})
+    })
+  })
+
+  it('save', async () => {
+    const now = new Date()
+    const lastRun = {
+      'owner/repo': {
+        lastRun: 100,
+        updatedAt: now,
+      }
+    }
+    await localStore.write(lastRun)
+
+    const readFile = await fs.promises.readFile(storePath)
+    const actual = JSON.parse(readFile.toString())
+
+    expect(actual).toStrictEqual({
+      'owner/repo': {
+        lastRun: 100,
+        updatedAt: expect.anything()
+      }
+    })
+  })
+})
