296e6ff2715e5374d5e7809c6dd16ebd3f2133f6
Kenta Kase <kesin1202000@gmail.com>
2020-05-26 01:12:16 +0900

    deps: Add jest-junit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      suiteName: "CIAnalyzer tests",
+      outputDirectory: "junit",
+      usePathForSuiteName: "true",
+      classNameTemplate: "{classname}",
+      titleTemplate: "{title}"
+    }]
+    // 'jest-junit'
+  ],
   testEnvironment: 'node',
 };
