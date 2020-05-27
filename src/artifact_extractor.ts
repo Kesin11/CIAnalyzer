cccd178f4fcb2893a4d037c5e7ee453ea6165ece
Kenta Kase <kesin1202000@gmail.com>
2020-05-27 15:01:14 +0900

    chore: Add ArtifactExtractor

---
+++ src/artifact_extractor.ts
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import AdmZip from 'adm-zip'
+import crypto from 'crypto'
+import minimatch from 'minimatch'
+
+const createRandomStr = () => crypto.randomBytes(8).toString('hex')
+
+export class ArtifactExtractor {
+  zipPaths: string[] = []
+  constructor() { }
+
+  async rmTmpZip() {
+    for (const zipPath of this.zipPaths) {
+      try {
+        await fs.promises.access(zipPath)
+        await fs.promises.unlink(zipPath)
+      } catch (_error) {
+        console.error(`Error: Could not remove ${zipPath}`)
+      }
+    }
+    this.zipPaths = []
+  }
+
+  async put(zipName: string, arraybuffer: ArrayBuffer) {
+    const zipPath = path.join(os.tmpdir(), `cianalyzer-${createRandomStr()}-${zipName}`)
+    await fs.promises.writeFile(zipPath, Buffer.from(arraybuffer))
+    this.zipPaths.push(zipPath)
+  }
+
+  extract(globs: string[]): AdmZip.IZipEntry[] {
+    const zipEntries = this.zipPaths.flatMap((zipPath) => {
+      return new AdmZip(zipPath).getEntries()
+    })
+
+    return zipEntries
+      .filter((entry) => {
+        return !entry.isDirectory &&
+          globs.some((glob) => minimatch(entry.entryName, glob))
+      })
+      // .forEach((zipEntry) => {
+      //   console.log(zipEntry.toString()) // outputs zip entries information
+      //   console.log(zipEntry.entryName) // "zip_test/nest/nest.txt"
+      //   console.log(zipEntry.name) // next.txt
+      //   if (!zipEntry.isDirectory) console.log(zipEntry.getData().toString('utf8'))
+      // })
+  }
+}
