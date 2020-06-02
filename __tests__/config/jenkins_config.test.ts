cc7f328fe11d90dfa0caaaf11a70268a48f8cdbc
Kenta Kase <kesin1202000@gmail.com>
2020-06-02 22:55:33 +0900

    test: Fix parse config tests

--- __tests__/config/jenkins_config.test.ts
+++ __tests__/config/jenkins_config.test.ts
@@ -1,18 +1,37 @@
 import { parseConfig } from '../../src/config/jenkins_config'
 
 describe('parseConfig', () => {
-  describe('repos', () => {
-    it('should have valid job names', () => {
-      const config = {
-        configDir: __dirname,
-        jenkins: {
-          baseUrl: 'http://localhost:8080',
-          jobs: [ 'sample-job' ]
-        }
+  it('when repos are string', () => {
+    const config = {
+      configDir: __dirname,
+      jenkins: {
+        baseUrl: 'http://localhost:8080',
+        jobs: [ 'sample-job' ]
       }
+    }
 
-      const actual = parseConfig(config)
-      expect(actual).toEqual(config.jenkins)
+    const actual = parseConfig(config)
+    expect(actual).toEqual(config.jenkins)
+  })
+
+  it('when repos are object', () => {
+    const config = {
+      configDir: __dirname,
+      jenkins: {
+        baseUrl: 'http://localhost:8080',
+        jobs: [{
+          name: 'sample-job',
+          tests: ['**/*.xml']
+        }]
+      }
+    }
+
+    const actual = parseConfig(config)
+    expect(actual).toEqual({
+      baseUrl: 'http://localhost:8080',
+      jobs: [{
+        name: 'sample-job', testGlob: ['**/*.xml']
+      }]
     })
   })
 })
