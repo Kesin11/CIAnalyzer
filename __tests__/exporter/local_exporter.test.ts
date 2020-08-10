5eee10cd7649e92a401a1550364bf539968813c2
Kenta Kase <kesin1202000@gmail.com>
2020-08-10 18:10:13 +0900

    test: Add local_exporter test

--- __tests__/exporter/local_exporter.test.ts
+++ __tests__/exporter/local_exporter.test.ts
@@ -2,6 +2,11 @@ import { LocalExporter } from '../../src/exporter/local_exporter'
 import path from 'path'
 import { LocalExporterConfig } from '../../src/config/config'
 
+const mockFsPromises = {
+  mkdir: jest.fn(),
+  writeFile: jest.fn(),
+}
+
 describe('LocalExporter', () => {
   describe('new', () => {
     it('should all property set with default parameter when options is null', async () => {
@@ -32,6 +37,30 @@ describe('LocalExporter', () => {
       const exporter = new LocalExporter('github', configDir, config )
 
       expect(exporter.formatter).toEqual(exporter.formatJsonLines)
+    })
+  })
+  describe('export', () => {
+    const report = [{}]
+    const config: LocalExporterConfig = { outDir: 'testTmp' }
+    const configDir = __dirname
+    let exporter: LocalExporter
+    beforeEach(() => {
+      exporter = new LocalExporter('github', configDir, config)
+      exporter.fsPromises = mockFsPromises as any
+    })
+
+    it('exportWorkflowReports create {DATE}-workflow-{SERVICE}.json', async () => {
+      await exporter.exportWorkflowReports(report as any)
+
+      expect(mockFsPromises.writeFile).toHaveBeenCalled()
+      expect(mockFsPromises.writeFile.mock.calls[0][0]).toMatch(/.+-workflow-github.json/)
+    })
+
+    it('exporTestReports create {DATE}-test-{SERVICE}.json', async () => {
+      await exporter.exportTestReports(report as any)
+
+      expect(mockFsPromises.writeFile).toHaveBeenCalled()
+      expect(mockFsPromises.writeFile.mock.calls[0][0]).toMatch(/.+-test-github.json/)
     })
   })
 })
