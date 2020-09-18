5eb638e17e64404ed8c4fa09e693181bc268341b
Kenta Kase <kesin1202000@gmail.com>
2020-09-18 09:39:09 +0900

    fix: Show more detail in error log

--- src/analyzer/github_analyzer.ts
+++ src/analyzer/github_analyzer.ts
@@ -173,7 +173,7 @@ export class GithubAnalyzer implements Analyzer {
           successCount: (testSuites.failures && testSuites.failures > 0) ? 0 : 1,
         })
       } catch (error) {
-        console.error(`Error: Could not parse as JUnit XML. ${artifact.path}`)
+        console.error(`Error: Could not parse as JUnit XML. workflowRunId: ${workflowReport.workflowRunId}, path: ${artifact.path}`)
       }
     }
     return testReports
