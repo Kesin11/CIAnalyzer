f170277c5a7d568b6b8136402a8102a391fd941f
Kenta Kase <kesin1202000@gmail.com>
2020-11-25 00:24:23 +0900

    WIP: Fetch Bitrise builds

---
+++ src/analyzer/bitrise_analyzer.ts
@@ -0,0 +1,116 @@
+import { Analyzer, diffSec, Status, TestReport, WorkflowParams, convertToReportTestSuites } from './analyzer'
+import { BuildResponse, BuildLogResponse, App, BitriseStatus } from '../client/bitrise_client'
+
+type WorkflowReport = {
+  // workflow
+  service: 'bitrise',
+  workflowId: string, // = ${app.fullname}-${triggered_workflow}
+  workflowRunId: string // = ${app.fullname}-${triggered_workflow}-${build_number}
+  buildNumber: number, // = build_number
+  workflowName: string, // = triggered_workflow
+  createdAt: Date, // = triggered_at
+  trigger: string // = triggered_by
+  status: Status,
+  repository: string, // = app.fullname
+  headSha: string, // = commit_hash
+  branch: string,
+  tag: string,
+  jobs: JobReport[],
+  startedAt: Date, // = started_on_worker_at
+  completedAt: Date // = finished_at
+  workflowDurationSec: number // = completedAt - startedAt
+  sumJobsDurationSec: number // = sum(jobs sumStepsDurationSec)
+  successCount: 0 | 1 // = 'SUCCESS': 1, others: 0
+  parameters: [] // BitriseAnalyzer does not support build parameters yet
+  queuedDurationSec: number // createdAt - startedAt
+}
+
+type JobReport = {
+  workflowRunId: string, // = workflowRunId
+  buildNumber?: number, // undefined. Bitrise Action does not provide job build number
+  jobId: string, // = id
+  jobName: string,
+  status: Status,
+  startedAt: Date,
+  completedAt: Date,
+  jobDurationSec: number, // = completedAt - startedAt
+  sumStepsDurationSec: number // = sum(steps duration)
+  steps: StepReport[],
+}
+
+type StepReport = {
+  name: string,
+  status: Status,
+  number: number,
+  startedAt: Date,
+  completedAt: Date,
+  stepDurationSec: number // completedAt - startedAt
+}
+
+type JobParameter = {
+  name: string
+  value: string
+}
+
+export class BitriseAnalyzer implements Analyzer {
+  constructor() { }
+
+  createWorkflowParams(app: App, build: BuildResponse): WorkflowParams {
+    return {
+      workflowId: `${app.fullname}-${build.triggered_workflow}`,
+      workflowRunId: `${app.fullname}-${build.triggered_workflow}-${build.build_number}`,
+      buildNumber: build.build_number,
+      workflowName: build.triggered_workflow,
+    }
+  }
+
+  createWorkflowReport(app: App, build: BuildResponse, buildLog: BuildLogResponse): WorkflowReport {
+    const { workflowId, workflowRunId, buildNumber, workflowName }
+      = this.createWorkflowParams(app, build)
+    const createdAt = new Date(build.triggered_at)
+    const startedAt = new Date(build.started_on_worker_at)
+    const completedAt = new Date(build.finished_at)
+    const status = this.normalizeStatus(build.status)
+    return {
+      service: 'bitrise',
+      workflowId,
+      workflowRunId,
+      buildNumber,
+      workflowName,
+      createdAt: new Date(build.triggered_at),
+      trigger: build.triggered_by,
+      status,
+      repository: app.fullname,
+      headSha: build.commit_hash ?? '',
+      branch: build.branch,
+      tag: build.tag ?? '',
+      jobs: [], // TODO
+      startedAt: new Date(build.started_on_worker_at),
+      completedAt: new Date(build.finished_at),
+      workflowDurationSec: diffSec(startedAt, completedAt),
+      sumJobsDurationSec: 0, // TODO
+      successCount: (status === 'SUCCESS') ? 1 : 0,
+      parameters: [],
+      queuedDurationSec: diffSec(createdAt, startedAt)
+    }
+  }
+
+  normalizeStatus(status: BitriseStatus): Status {
+    switch (status) {
+      case 1:
+        return 'SUCCESS'
+      case 2:
+        return 'FAILURE'
+      case 3:
+        return 'ABORTED'
+      case 4:
+        return 'ABORTED'
+      default:
+        return 'OTHER';
+    }
+  }
+
+  async createTestReports(): Promise<TestReport[]> {
+    return []
+  }
+}
---
+++ src/runner/bitrise_runner.ts
@@ -0,0 +1,100 @@
+import { maxBy } from "lodash"
+import { WorkflowReport } from "../analyzer/analyzer"
+import { BitriseAnalyzer } from "../analyzer/bitrise_analyzer"
+import { BitriseClient } from "../client/bitrise_client"
+import { BitriseConfig, parseConfig } from "../config/bitrise_config"
+import { YamlConfig } from "../config/config"
+import { CompositExporter } from "../exporter/exporter"
+import { LastRunStore } from "../last_run_store"
+import { Result, success, failure } from "../result"
+import { Runner } from "./runner"
+
+export class BitriseRunner implements Runner {
+  service: string = 'bitrise'
+  client: BitriseClient
+  analyzer: BitriseAnalyzer
+  configDir: string
+  config: BitriseConfig | undefined
+  store?: LastRunStore
+  constructor(public yamlConfig: YamlConfig) {
+    const BITRISE_TOKEN = process.env['BITRISE_TOKEN'] || ''
+    this.configDir = yamlConfig.configDir
+    this.config = parseConfig(yamlConfig)
+    this.client = new BitriseClient(BITRISE_TOKEN)
+    this.analyzer = new BitriseAnalyzer()
+  }
+
+  private setRepoLastRun(reponame: string, reports: WorkflowReport[]) {
+    const lastRunReport = maxBy(reports, 'buildNumber')
+    if (lastRunReport) {
+      this.store?.setLastRun(reponame, lastRunReport.buildNumber)
+    }
+  }
+
+  async run (): Promise<Result<void, Error>> {
+    let result: Result<void, Error> = success()
+    if (!this.config) return failure(new Error('this.config must not be undefined'))
+    this.store = await LastRunStore.init(this.service, this.configDir, this.config.lastRunStore)
+
+    let workflowReports: WorkflowReport[] = []
+    // let testReports: TestReport[] = []
+    // const customReportCollection = new CustomReportCollection()
+
+    const allApps = await this.client.fetchApps()
+    const appRepos = this.config.repos.map((repo) => {
+      // TODO: Bitriseのappのfullnameが重複しているケースの考慮を追加
+      // allAppsの方でgroupByして、length > 1のケースでslugがundefinedのケースをエラーでおとす
+      const app = allApps.find((app) => repo.slug
+        ? repo.slug === app.slug
+        : repo.fullname === app.fullname )! // TODO: undefinedのケースは後で考える
+      return { app, repo }
+    })
+
+    for (const { app, repo } of appRepos) {
+      console.info(`Fetching ${this.service} - ${repo.fullname} ...`)
+
+      try {
+        const lastRunId = this.store.getLastRun(app.slug)
+        const builds = await this.client.fetchBuilds(app.slug, lastRunId)
+
+        for (const build of builds) {
+          // Fetch data
+          const buildLog = await this.client.fetchJobLog(app.slug, build.slug)
+          // const tests = await this.client.fetchTests(repo.owner, repo.repo, workflowRun.run.id, repo.testGlob)
+          // const customReportArtifacts = await this.client.fetchCustomReports(repo.owner, repo.repo, workflowRun.run.id, repo.customReports)
+
+          // Create report
+          const workflowReport = this.analyzer.createWorkflowReport(app, build, buildLog)
+          // const testReports = await this.analyzer.createTestReports(workflowReport, tests)
+          // const runCustomReportCollection = await createCustomReportCollection(workflowReport, customReportArtifacts)
+
+          // Aggregate
+          workflowReports.push(workflowReport)
+          // repoTestReports = repoTestReports.concat(testReports)
+          // customReportCollection.aggregate(runCustomReportCollection)
+        }
+      }
+      catch (error) {
+        const errorMessage = `Some error raised in '${repo.fullname}', so it skipped.`
+        console.error(errorMessage)
+        console.error(error)
+        result = failure(new Error(errorMessage))
+        continue
+      }
+      this.setRepoLastRun(repo.fullname, workflowReports)
+      // workflowReports = workflowReports.concat(repoWorkflowReports)
+      // testReports = testReports.concat(repoTestReports)
+    }
+
+    console.info(`Exporting ${this.service} workflow reports ...`)
+    const exporter = new CompositExporter(this.service, this.configDir, this.config.exporter)
+    await exporter.exportWorkflowReports(workflowReports)
+    // await exporter.exportTestReports(testReports)
+    // await exporter.exportCustomReports(customReportCollection)
+
+    this.store.save()
+    console.info(`Done execute '${this.service}'. status: ${result.type}`)
+
+    return result
+  }
+}
