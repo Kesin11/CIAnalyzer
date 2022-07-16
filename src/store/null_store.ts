91361ebdd660bc5113b5921bdd69757e72d5c9cf
Kenta Kase <kesin1202000@gmail.com>
2022-07-16 15:37:21 +0900

    refactor: Replace TS private to Nodejs hard private

--- src/store/null_store.ts
+++ src/store/null_store.ts
@@ -2,19 +2,19 @@ import { Logger } from 'tslog'
 import { Store, AnyObject } from './store'
 
 export class NullStore implements Store {
-  private logger: Logger
+  #logger: Logger
 
   constructor(logger: Logger) {
-    this.logger = logger.getChildLogger({ name: NullStore.name })
+    this.#logger = logger.getChildLogger({ name: NullStore.name })
   }
 
   async read<T extends AnyObject>(): Promise<T> {
-    this.logger.info(`Detect DEBUG mode, nothing is used instead.`)
+    this.#logger.info(`Detect DEBUG mode, nothing is used instead.`)
     return {} as T
   }
 
   async write<T extends AnyObject> (newStore: T): Promise<T> {
-    this.logger.info(`Detect DEBUG mode, skip saving lastRun.`)
+    this.#logger.info(`Detect DEBUG mode, skip saving lastRun.`)
     return {} as T
   }
 }
