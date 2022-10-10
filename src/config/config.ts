d19794e5962b66a179af21093cc85aba19254358
Kenta Kase <kesin1202000@gmail.com>
2022-10-11 00:35:17 +0900

    refactor: Replace common config types with zod

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,59 +1,70 @@
 import fs from 'fs'
 import yaml from 'js-yaml'
 import { Logger } from 'tslog'
+import { z } from 'zod'
 
-export type YamlConfig = {
-  github?: { [key: string]: unknown }
-  circleci?: { [key: string]: unknown }
-  jenkins?: { [key: string]: unknown }
-  bitrise?: { [key: string]: unknown }
-}
+const yamlSchema = z.object({
+  github: z.record(z.unknown()).optional(),
+  circleci: z.record(z.unknown()).optional(),
+  jenkins: z.record(z.unknown()).optional(),
+  bitrise: z.record(z.unknown()).optional(),
+})
+export type YamlConfig = z.infer<typeof yamlSchema>
 
-export type CommonConfig = {
-  baseUrl?: string
-  exporter?: ExporterConfig
-  lastRunStore?: LastRunStoreConfig
-}
+const localExporterSchema = z.object({
+  outDir: z.string().optional(),
+  format: z.union([z.literal('json'), z.literal('json_lines')]).optional()
+})
+export type LocalExporterConfig = z.infer<typeof localExporterSchema>
 
-export type ExporterConfig = {
-  local: LocalExporterConfig | null
-  bigquery: BigqueryExporterConfig | null
-}
+const bigqueryExporterSchema = z.object({
+  project: z.string().optional(),
+  dataset: z.string().optional(),
+  reports: z.object({
+    name: z.union([z.literal('workflow'), z.literal('test_report')]),
+    table: z.string(),
+  }).array().optional(),
+  customReports: z.object({
+    name: z.string(),
+    table: z.string(),
+    schema: z.string(),
+  }).array().optional(),
+  maxBadRecords: z.number().optional()
+})
+export type BigqueryExporterConfig = z.infer<typeof bigqueryExporterSchema>
 
-export type LocalExporterConfig = {
-  outDir?: string
-  format?: 'json' | 'json_lines'
-}
+const exporterSchema = z.object({
+  local: localExporterSchema.optional(),
+  bigquery: bigqueryExporterSchema.optional()
+})
+export type ExporterConfig = z.infer<typeof exporterSchema>
 
-export type BigqueryExporterConfig = {
-  project?: string
-  dataset?: string
-  reports?: {
-    name: 'workflow' | 'test_report'
-    table: string
-  }[]
-  customReports?: {
-    name: string
-    table: string
-    schema: string
-  }[]
-  maxBadRecords?: number
-}
+const lastRunStoreSchema = z.union([
+  z.object({
+    backend: z.literal('local'),
+    path: z.string()
+  }),
+  z.object({
+    backend: z.literal('gcs'),
+    project: z.string(),
+    bucket: z.string(),
+    path: z.string().optional()
+  })
+])
+export type LastRunStoreConfig = z.infer<typeof lastRunStoreSchema>
 
-export type LastRunStoreConfig = {
-  backend: 'local'
-  path: string
-} | {
-  backend: 'gcs'
-  project: string
-  bucket: string
-  path?: string
-}
+const customReportSchema = z.object({
+  name: z.string(),
+  paths: z.string().array()
+})
+export type CustomReportConfig = z.infer<typeof customReportSchema>
 
-export type CustomReportConfig = {
-  name: string
-  paths: string[]
-}
+const commonConfig= z.object({
+  baseUrl: z.string().optional(),
+  exporter: exporterSchema.optional(),
+  lastRunStore: lastRunStoreSchema.optional()
+})
+export type CommonConfig = z.infer<typeof commonConfig>
 
 export const loadConfig = (logger: Logger, configPath: string): YamlConfig => {
   const config = yaml.load(fs.readFileSync(configPath, 'utf8'))
