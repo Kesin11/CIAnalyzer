13f0862dcd9a6311086b69515c2ce204eba2d511
Kenta Kase <kesin1202000@gmail.com>
2023-06-17 14:37:47 +0900

    Add validateConfig()

---
+++ __tests__/config/validator.test.ts
@@ -0,0 +1,60 @@
+import { Logger } from 'tslog'
+import { ValidatedYamlConfig, validateConfig } from '../../src/config/validator'
+
+const logger = new Logger({ type: "hidden" })
+
+describe('validateConfig', () => {
+  describe('when valid', () => {
+    it('should return the validated config with ValidatedYamlConfig types', () => {
+      const config = {
+        github: {
+          repos: ["owner/repo"],
+        }
+      }
+
+      const validatedConfig = validateConfig(logger, config, true);
+
+      expect(validatedConfig).toEqual({
+        ...config,
+        _configValidated: true,
+      } as ValidatedYamlConfig)
+    })
+  })
+  
+  describe('when invalid', () => {
+    it('has extra property', () => {
+      const config = {
+        github: {
+          foobar: true, // extra property
+          repos: ["owner/repo"],
+        }
+      }
+      
+      expect(() => validateConfig(logger, config, true)).toThrow();
+    })
+
+    it('has lacking required property', () => {
+      const config = {
+        github: {
+          repos: [{
+            // name: "owner/repo",
+            tests: ["**/*.xml"],
+          }]
+        }
+      } as any
+      
+      expect(() => validateConfig(logger, config, true)).toThrow();
+    })
+
+    it('has wrong types', () => {
+      const config = {
+        github: {
+          repos: [{
+            name: 12 // Invalid types
+          }]
+        }
+      } as any
+      expect(() => validateConfig(logger, config, true)).toThrow();
+    })
+  })
+})
