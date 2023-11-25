f172c212fd820c043ee81ca42d667272546fa531
Kenta Kase <kesin1202000@gmail.com>
2023-11-25 03:30:52 +0000

    bugfix: BigQueryExporter does not work with esbuild

---
+++ src/constant.ts
@@ -0,0 +1,8 @@
+import path from "node:path"
+
+// NOTE: Relative path using __dirname is different between esbuild and ts-node
+// Therefore, define the paths to the files in constant.ts placed in the same directory as index.ts.
+export const BQ_SCHEMA_PATHS = {
+  workflow: path.join(__dirname, '..', 'bigquery_schema/workflow_report.json'),
+  test_report: path.join(__dirname, '..', 'bigquery_schema/test_report.json'),
+}
